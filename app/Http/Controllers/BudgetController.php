804048647efe745ff2e452ca9a9581ca00905c65
davidk <dkato81@gmail.com>
2024-03-31 15:19:01 +0300

    enable api route

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Budget;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $budgets = Budget::all();
+
+        return response()->json($budgets);
+    }
 }
